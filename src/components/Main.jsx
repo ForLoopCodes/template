7b7e24eefd01fda7f22489ca65c2c9e9be3a9999
ForLoop <meetnp1706@gmail.com>
2023-09-29 17:20:15 +0530

    first commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './elements/header'
+
+export default function Main() {
+    return (
+        <>
+        <div>
+            <Header/>
+        </div>
+        </>
+        )
+}
---
+++ src/components/elements/header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+export default function Header(props) {
+    return (
+        <>
+            <div>
+                hello
+            </div>
+        </>
+    )
+}
